9d0e6693407af2f27d711f6ed5063d71bc129bb1
igoor <igorwallacy3@gmail.com>
2024-08-07 20:42:05 -0300

    recomendados

--- src/app/_pages/_recomendados-banner.tsx
+++ src/app/_pages/_recomendados-banner.tsx
@@ -12,13 +12,13 @@ const Re_Banner = async () => {
 
     return (
         <>
-            <main className='' >
+            <main className='lg:px-0 lg:py-0 pt-5 px-5' >
                 <div>
                     <h1 className='uppercase text-sm' >Recomendados</h1>
                 </div>
                 <div className='flex gap-3 mt-5' >
                     {
-                        barbershop.slice(0, 3).map((barber) => (
+                        barbershop.slice(0, 2).map((barber) => (
                             <BarbershopItem key={barber.id} barbershop={barber} />
                         ))
                     }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Header from './_pages/_header'
 import Banner_Main from './_pages/_banner-main'
 import Search_Home from './_pages/components/_search-home'
 import Popular from './_pages/_popular'
+import Re_Banner from './_pages/_recomendados-banner'
 
 const Page = () => {
   return (
@@ -12,6 +13,7 @@ const Page = () => {
       <Header />
       <Banner_Main />
       <div className='block lg:hidden px-5 pt-5' ><Search_Home /></div>
+      <div className='block lg:hidden' ><Re_Banner /></div>
       <Popular />
     </>
   )
