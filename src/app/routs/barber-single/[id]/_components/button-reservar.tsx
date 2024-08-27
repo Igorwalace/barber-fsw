a5d517d91835bbeba431f7aa0d0b802e5d233d9a
igoor <igorwallacy3@gmail.com>
2024-08-27 20:42:49 -0300

    date

--- src/app/routs/barber-single/[id]/_components/button-reservar.tsx
+++ src/app/routs/barber-single/[id]/_components/button-reservar.tsx
@@ -2,6 +2,9 @@
 
 import React, { useState } from 'react'
 
+//local
+import { ptBR } from "date-fns/locale";
+
 //shadcn
 import { Button } from '@/app/_services/components/ui/button'
 import {
@@ -10,7 +13,6 @@ import {
     SheetDescription,
     SheetHeader,
     SheetTitle,
-    SheetTrigger,
 } from "@/app/_services/components/ui/sheet"
 
 //prisma
@@ -24,11 +26,39 @@ interface BarberServiceProps {
 
 const Button_Reservar = ({ barberservice }: BarberServiceProps) => {
 
+    const TIME_LIST = [
+        "08:00",
+        "08:30",
+        "09:00",
+        "09:30",
+        "10:00",
+        "10:30",
+        "11:00",
+        "11:30",
+        "12:00",
+        "12:30",
+        "13:00",
+        "13:30",
+        "14:00",
+        "14:30",
+        "15:00",
+        "15:30",
+        "16:00",
+        "16:30",
+        "17:00",
+        "17:30",
+        "18:00",
+    ]
+
     const [openReserve, useOpenReserve] = useState(false)
-    const [date, setDate] = useState<Date | undefined>(new Date())
+    const [date, setDate] = useState<Date | undefined>(undefined)
 
     const HandleReservar = (id: string) => {
         useOpenReserve(true)
+    }
+
+    const handleSelectDay = (dateSelect: Date | undefined) => {
+        setDate(dateSelect)
     }
 
     return (
@@ -36,21 +66,72 @@ const Button_Reservar = ({ barberservice }: BarberServiceProps) => {
             <Button onClick={() => HandleReservar(barberservice.id)} variant='secondary' >Reservar</Button>
 
             <Sheet open={openReserve} onOpenChange={useOpenReserve} >
-                <SheetContent>
+                <SheetContent className='overflow-y-auto [&::-webkit-scrollbar]:hidden' >
                     <SheetHeader>
                         <SheetTitle className='text-lg mb-3' >Fazer Reservar - {barberservice.name}</SheetTitle>
                         <Separator />
                     </SheetHeader>
-                    <SheetDescription className='mt-5' >
-                        <div className='flex justify-center' >
+                    <SheetDescription className='w-full' >
+                        <div className="border-b border-solid py-5">
                             <Calendar
                                 mode="single"
-                                // locale={ptBR}
+                                locale={ptBR}
                                 selected={date}
-                                onSelect={setDate}
+                                onSelect={handleSelectDay}
                                 fromDate={new Date()}
+                                styles={{
+                                    head: {
+                                        width: "100%",
+                                        textTransform: "capitalize",
+                                    },
+                                    cell: {
+                                        width: "100%",
+                                    },
+                                    button: {
+                                        width: "100%",
+                                    },
+                                    nav_button_previous: {
+                                        width: "32px",
+                                        height: "32px",
+                                    },
+                                    nav_button_next: {
+                                        width: "32px",
+                                        height: "32px",
+                                    },
+                                    caption: {
+                                        textTransform: "capitalize",
+                                    },
+                                }}
                             />
                         </div>
+                        {
+                            date &&
+                            <div className="lg:grid grid-cols-4 py-5 justify-center flex gap-3 overflow-x-auto pl-0 [&::-webkit-scrollbar]:hidden">
+                                {
+                                    TIME_LIST.length > 0 &&
+                                    TIME_LIST.map((time) => (
+                                        <Button
+                                            key={time}
+                                            variant="outline"
+                                            className="rounded-full"
+                                        // onClick={() => handleTime\elect(time)}
+                                        >
+                                            {time}
+                                        </Button>
+                                    ))
+                                }
+                            </div>
+                        }
+                        {
+                            !date &&
+                            <div className='py-5 pl-2' >
+                                <p className="text-xs">
+                                    Não há horários disponíveis para este dia.
+                                </p>
+                            </div>
+                        }
+
+                        <Separator />
                     </SheetDescription>
                 </SheetContent>
             </Sheet>
